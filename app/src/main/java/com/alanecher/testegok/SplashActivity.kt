117ca7e0b844bab1b7214d57f1b3af6a248d24d7
Alan Echer <alan.echer@gmail.com>
2020-09-16 17:03:27 -0300

    splash screen com logo e tela principal

---
+++ app/src/main/java/com/alanecher/testegok/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.alanecher.testegok
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIMEOUT: Long = 1400
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, SPLASH_TIMEOUT)
+    }
+}
